68fa7ae963ed08df274e99b36dcef4ae0f70032e
Sebastien Siva <ssiva@ggc.edu>
2021-03-27 08:02:07 -0400

    SillySort Source and WebBuild

---
+++ SillySort/Unity_Project/SillySort/Assets/CardController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardController : MonoBehaviour
+{
+    public Text num, reveal, left, right;
+    bool isHidden;
+    int value;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isHidden = true;
+        int i = transform.GetSiblingIndex();
+        value = GameMan.numList[i];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isHidden)
+            num.text = "?";
+        else
+            num.text = "" + value;
+
+        reveal.GetComponentInParent<Button>().interactable = (
+            GameMan.numRevealed < GameMan.maxRevealed || !isHidden);
+
+        left.GetComponentInParent<Button>().interactable = (
+            transform.GetSiblingIndex() > 0);
+
+        right.GetComponentInParent<Button>().interactable = (
+            transform.GetSiblingIndex() < (transform.parent.childCount -1));
+
+    }
+
+    public void revealToggle()
+    {
+        if (isHidden)
+        {
+            if (GameMan.numRevealed < GameMan.maxRevealed)
+            {
+                GameMan.numRevealed++;
+                isHidden = false;
+                reveal.text = "Hide";
+            }
+        }
+        else
+        {
+            GameMan.numRevealed--;
+            isHidden = true;
+            reveal.text = "Reveal";
+        }
+    }
+
+    public void move(int dir)
+    {
+        int i = transform.GetSiblingIndex();
+        transform.SetSiblingIndex(i + dir);
+    }
+
+
+}
---
+++ SillySort/Unity_Project/SillySort/Assets/GameMan.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMan : MonoBehaviour
+{
+    public static List<int> numList;
+    public static int numRevealed;
+    public const int maxRevealed = 2;
+    void Awake()
+    {
+        numList = new List<int>();
+        for (int i = 0; i < 100; i++)
+            numList.Add(i);
+
+        numList.Shuffle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
